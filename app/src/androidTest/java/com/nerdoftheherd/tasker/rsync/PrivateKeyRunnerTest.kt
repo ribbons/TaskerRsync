a84ac2514009183a61cb28286257e11a938e60c2
Matt Robinson <git@nerdoftheherd.com>
2021-08-23 20:00:26 +0100

    Add simple instrumented tests for PrivateKeyRunner

---
+++ app/src/androidTest/java/com/nerdoftheherd/tasker/rsync/PrivateKeyRunnerTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2021 Matt Robinson
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nerdoftheherd.tasker.rsync
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.joaomgcd.taskerpluginlibrary.input.TaskerInput
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.greaterThan
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.File
+
+@RunWith(AndroidJUnit4::class)
+class PrivateKeyRunnerTest {
+    @Test
+    fun generateKey() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val config = TaskerInput(PrivateKeyConfig())
+
+        val keyFile = File(context.filesDir, "id_dropbear")
+        keyFile.delete()
+
+        val result = PrivateKeyRunner().run(context, config)
+        assertTrue(result.success)
+        assertTrue(keyFile.exists())
+    }
+
+    @Test
+    fun keyExists() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val config = TaskerInput(PrivateKeyConfig())
+
+        val keyFile = File(context.filesDir, "id_dropbear")
+        keyFile.createNewFile()
+
+        val result = PrivateKeyRunner().run(context, config)
+        assertFalse(result.success)
+    }
+
+    @Test
+    fun keyExistsOverwrite() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val config = TaskerInput(PrivateKeyConfig("Ed25519", "256", true))
+
+        val keyFile = File(context.filesDir, "id_dropbear")
+        keyFile.createNewFile()
+        keyFile.setLastModified(0)
+
+        val result = PrivateKeyRunner().run(context, config)
+        assertTrue(result.success)
+        assertTrue(keyFile.exists())
+        assertThat(keyFile.lastModified(), greaterThan(0))
+    }
+}
