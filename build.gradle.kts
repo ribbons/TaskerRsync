f0ba69f68056a3b70005b96516808ef35e76c97b
Matt Robinson <git@nerdoftheherd.com>
2021-09-12 14:07:22 +0100

    Convert Gradle build scripts from Groovy to Kotlin
    This should make the scripts more maintainable in future as Kotlin
    Script has better IDE support (e.g. showing deprecated APIs) and is
    generally nicer than Groovy.  Also change use of deprecated APIs to
    their replacements and switch to using KotlinCompilerVersion instead of
    a variable for setting the Kotlin version in several places.

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2021 Matt Robinson
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    }
+}
+
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/ribbons/android-rsync")
+            credentials {
+                username = project.findProperty("gpr.user") as String?
+                    ?: System.getenv("PKG_USERNAME")
+                password = project.findProperty("gpr.key") as String?
+                    ?: System.getenv("PKG_TOKEN")
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+/*
+ * Copyright © 2021 Matt Robinson
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+rootProject.name = "Rsync for Tasker"
+include(":app")
