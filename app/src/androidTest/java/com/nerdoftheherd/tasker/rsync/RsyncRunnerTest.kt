5dd0fe41826b24dffa8661072a7ad685f0cebc4b
Matt Robinson <git@nerdoftheherd.com>
2021-09-19 15:52:31 +0100

    Declare permissions to read/write shared storage
    For Android 10 and below, declare the WRITE_EXTERNAL_STORAGE permission.
    This allows both reading from and writing to shared storage (with the
    addition of requestLegacyExternalStorage=true just for Android 10).
    
    For Android 11 and above, declare the MANAGE_EXTERNAL_STORAGE
    permission.  This will require additional approval through the Play
    Store but is the only way that the app can read and write to parts of
    the shared storage which are outside the app's defined storage path.
    
    Add an instrumented test that both reading from and writing to the
    primary shared storage is successful and run the tests under Android 4.4
    and 11 in addition to Android 10 on GitHub Actions.

--- app/src/androidTest/java/com/nerdoftheherd/tasker/rsync/RsyncRunnerTest.kt
+++ app/src/androidTest/java/com/nerdoftheherd/tasker/rsync/RsyncRunnerTest.kt
@@ -7,10 +7,13 @@
 package com.nerdoftheherd.tasker.rsync
 
 import android.content.Context
+import android.os.Build
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.joaomgcd.taskerpluginlibrary.input.TaskerInput
 import com.nerdoftheherd.tasker.rsync.config.RsyncConfig
+import junit.framework.TestCase.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
@@ -42,5 +45,35 @@ class RsyncRunnerTest {
 
         expecter.expect(RuntimeException::class.java)
         RsyncRunner().run(context, TaskerInput(config))
+    }
+
+    @Test
+    fun copyFromAndToPrimaryStorage() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            TestUtils.setManageStoragePermission(context, true)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            InstrumentationRegistry.getInstrumentation().uiAutomation.grantRuntimePermission(
+                context.packageName, android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        }
+
+        File(context.filesDir, "id_dropbear").createNewFile()
+
+        val pkgName = context.applicationContext.packageName
+        val sourceDir = File(TestUtils.primaryStorageDir(context), "$pkgName-source")
+        val targetDir = File(TestUtils.primaryStorageDir(context), "$pkgName-target")
+
+        sourceDir.mkdir()
+        File(sourceDir, "testfile").createNewFile()
+        targetDir.deleteRecursively()
+
+        val config = RsyncConfig("-r ${sourceDir.absolutePath}/ ${targetDir.absolutePath}/")
+        RsyncRunner().run(context, TaskerInput(config))
+
+        assertTrue(File(targetDir, "testfile").exists())
+        sourceDir.deleteRecursively()
+        targetDir.deleteRecursively()
     }
 }
---
+++ app/src/androidTest/java/com/nerdoftheherd/tasker/rsync/TestUtils.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2021 Matt Robinson
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.nerdoftheherd.tasker.rsync
+
+import android.app.AppOpsManager
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import android.os.Process
+import android.os.storage.StorageManager
+import androidx.test.platform.app.InstrumentationRegistry
+import java.io.File
+
+class TestUtils {
+    companion object {
+        fun primaryStorageDir(context: Context): File {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val manager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
+                val volume = manager.primaryStorageVolume
+                return volume.directory!!
+            }
+
+            // Fall back to using getExternalStorageDirectory even though it is
+            // deprecated as it appears to be the only way of getting this
+            // before Android 11
+            @Suppress("DEPRECATION")
+            return Environment.getExternalStorageDirectory()
+        }
+
+        fun setManageStoragePermission(context: Context, enabled: Boolean) {
+            val newstate = if (enabled) "allow" else "deny"
+
+            InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand(
+                "appops set --uid ${context.packageName} MANAGE_EXTERNAL_STORAGE $newstate"
+            )
+
+            val appOps = context.getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+
+            while (
+                appOps.unsafeCheckOpNoThrow(
+                    "android:manage_external_storage",
+                    Process.myUid(),
+                    context.packageName
+                ) != AppOpsManager.MODE_ALLOWED == enabled
+            ) {
+                Thread.sleep(1)
+            }
+        }
+    }
+}
