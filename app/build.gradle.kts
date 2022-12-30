9c5930d6d2c3dae95f60ceb70ac5ba09a9eda87b
Matt Robinson <git@nerdoftheherd.com>
2022-12-30 17:21:54 +0000

    Add integration test for update 'More Info' button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,4 +122,5 @@ dependencies {
     androidTestImplementation("androidx.test:core-ktx:1.5.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.0")
 }
--- app/src/androidTest/java/com/nerdoftheherd/tasker/rsync/activities/UpdateActivityTest.kt
+++ app/src/androidTest/java/com/nerdoftheherd/tasker/rsync/activities/UpdateActivityTest.kt
@@ -10,7 +10,12 @@ import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.espresso.intent.rule.IntentsRule
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -19,13 +24,18 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nerdoftheherd.tasker.rsync.R
 import com.nerdoftheherd.tasker.rsync.Version
 import com.nerdoftheherd.tasker.rsync.VersionInfo
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.net.URL
 
 @RunWith(AndroidJUnit4::class)
 class UpdateActivityTest {
+    @get:Rule
+    val intentsRule = IntentsRule()
+
     @Test
     fun newerVersion() {
         val versionInfo = VersionInfo(
@@ -68,6 +78,33 @@ class UpdateActivityTest {
             onView(withId(R.id.textSummary)).check(matches(withText(R.string.updated_summary)))
             onView(withId(R.id.buttonUpdate)).check(matches(not(isEnabled())))
             onView(withId(R.id.textInfo)).check(matches(not(isDisplayed())))
+        }
+    }
+
+    @Test
+    fun moreInfoViewsUrl() {
+        val versionInfo = VersionInfo(
+            Version("9999"),
+            URL("http://example.com/"),
+            URL("http://example.org/")
+        )
+
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            UpdateActivity::class.java
+        )
+
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        intent.putExtra("info", versionInfo)
+
+        launchActivity<UpdateActivity>(intent).use {
+            onView(withId(R.id.buttonMoreInfo)).perform(click())
+            intended(
+                allOf(
+                    hasAction(Intent.ACTION_VIEW),
+                    hasData(versionInfo.info.toString())
+                )
+            )
         }
     }
 }
